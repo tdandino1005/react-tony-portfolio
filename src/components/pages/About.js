0a014975c93246dd4403eaeed9aad8cc8ccb9067
Tony Dandino <tdandino1005@gmail.com>
2023-04-02 15:25:23 -0500

    Created and updated page about.js.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,14 +3,11 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Me</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        My name is Tony Dandino and I have been working in the hospitality industry for 20 years.
+        I have always been interested in web stack development and am taking this course to learn and further my knowledge and carreer.
+        I hope to get a job as a web developer or software engineer after this course to further my career.
       </p>
     </div>
   );
