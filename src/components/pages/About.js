69a08138bc9b4bc699644c5a170cff874dc2698c
Tony Dandino <tdandino1005@gmail.com>
2023-04-02 15:47:18 -0500

    Added content in about.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,9 +5,18 @@ export default function About() {
     <div>
       <h1>About Me</h1>
       <p>
+      </p>
+      <p>
         My name is Tony Dandino and I have been working in the hospitality industry for 20 years.
-        I have always been interested in web stack development and am taking this course to learn and further my knowledge and carreer.
-        I hope to get a job as a web developer or software engineer after this course to further my career.
+      <p>
+      <p>
+          I am hard working and dedicated to my job and I am looking to change careers to something that I am more passionate about.
+      </p>
+      <p>
+    I have always been interested in web stack development and am taking this course to learn and further my knowledge and career.
+      </p>
+      </p>
+        I hope to get a job as a web developer or software engineer after this course to work on creating projects that I am interested in.
       </p>
     </div>
   );
