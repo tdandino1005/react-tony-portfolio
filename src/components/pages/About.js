30371b54911a152a6085b107e83a0aac6f44e304
Tony Dandino <tdandino1005@gmail.com>
2023-04-03 13:38:06 -0500

    Added picture and styling to contact page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,23 +1,47 @@
 import React from 'react';
+import Tonypicture from '../../img/Tonypicture.jpg';
+
+const styles = {
+  Tonypicture: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    borderRadius: '50%',
+    maxHeight: '100%',
+    maxWidth: '100%',
+    border: '5px solid #0000FF',
+    cursor: 'default',
+
+  },
+  space: {
+    paddingLeft: '0.5rem',
+    paddingRight: '0.5rem',
+  },
+  h2: {
+    textAlign: 'center',
+  },
+};
+
 
 export default function About() {
   return (
-    <div>
-      <h1>About Me</h1>
-      <p>
-      </p>
+    <div style={styles.space}>
+      <h2 style={styles.h2}>About Me</h2>
+      <div>
+        <img style={styles.Tonypicture} src={Tonypicture} alt="Tony Dandino" />
+      </div>
       <p>
         My name is Tony Dandino and I have been working in the hospitality industry for 20 years.
-      <p>
+      </p>
       <p>
           I am hard working and dedicated to my job and I am looking to change careers to something that I am more passionate about.
       </p>
       <p>
     I have always been interested in web stack development and am taking this course to learn and further my knowledge and career.
       </p>
-      </p>
+      <p>
         I hope to get a job as a web developer or software engineer after this course to work on creating projects that I am interested in.
       </p>
     </div>
-  );
+  );  
 }
